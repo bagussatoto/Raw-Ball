27ea912bdc0a9aa0d4a4ebd1bb39dd8a01306b50
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2024-01-13 02:37:12 +0700

    fix : ``raw``

---
+++ component/assets.h
@@ -0,0 +1,31 @@
+#pragma once
+
+struct Plat
+{
+	double posX;
+	double posY;
+};
+
+struct Ball
+{
+	double posX;
+	double posY;
+	double velX;
+	double velY;
+	double radius;
+};
+
+struct Spike
+{
+	double posX;
+	double posY;
+
+};
+
+struct Sball
+{
+	double posX;
+	double posY;
+	double radius;
+};
+
